5c392ea8a01d8ec141ee9e20c2081fd07ad951fb
Rafael Karczevski <rkarczevski@gmail.com>
2022-04-06 20:55:27 -0400

    added function with error

--- server.go
+++ server.go
@@ -1,11 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    fmt.Printf("Starting server at port 8080\n")
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome!")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+/* Usado para forcar erro sonarcloud */
+func forError() {
+	if b == 0 {
+		log.Fatal("err")
+	} else {
+		log.Fatal("err")
+	}
 }
